204bf8030dcaba1b13e00143bc1145e91687f85e
mdshajib <mdshajib1@gmail.com>
2021-05-30 22:26:35 +0600

    home text

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,7 +2,7 @@
     <div class="home">
         <div class="home_content">
             <h2>Simple Employee CRUD</h2>
-            <h4>Using Laravel 8 with Sanctum authorization</h4>
+            <h4>Using Laravel 8 API with Sanctum authorization</h4>
         </div>
     </div>
 </template>
@@ -15,11 +15,15 @@ export default {
 <style>
 .home{
     position: relative;
+    width: 100%;
+    height: 100%;
+
 }
 .home_content{
     text-align: center;
     position: absolute;
-    top: 260px;
-    left: 340px;
+    top: 200px;
+    left: 0;
+    right: 0;
 }
 </style>
